e31d0ad996cb3a1880835a7ba0a0615319aba851
Cristina <cristinarp42@gmail.com>
2026-02-09 09:13:28 +0000

    Mis cambios en App.tsx antes del pull

--- EconomatoFront/src/App.tsx
+++ EconomatoFront/src/App.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { routes } from './app/router' 
 
-// Creamos el router aquí para que el main quede más corto
+
 const router = createBrowserRouter(routes);
 
 export default function App() {
