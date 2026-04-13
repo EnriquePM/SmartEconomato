c90fc886ea4d6b02ea17ccb2cdc55c8f8e00b2f9
Sergio <sergioprezort@gmail.com>
2026-04-13 10:35:54 +0100

    Update user.routes.ts

--- EconomatoBack/src/routes/user.routes.ts
+++ EconomatoBack/src/routes/user.routes.ts
@@ -7,10 +7,10 @@ const router = Router();
 // Rutas protegidas (todas requieren token)
 router.use(authenticateToken); // Opcional: aplicar a todas las de abajo de golpe
 
-router.get('/', requireRole(['Jefe_Economato', 'Profesor']), getUsers);             // Ver todos (Admin / Profesor)
+router.get('/', requireRole(['Jefe_Economato', 'Administrador', 'Profesor']), getUsers);             // Ver todos (Admin / Profesor)
 router.put('/:id', updateUser);                                            // Editar uno
-router.put('/:id/reset-password', requireRole(['Jefe_Economato', 'Profesor']), resetPassword); // Solo Jefe puede resetear contraseña
-router.put('/:id/role', requireRole(['Jefe_Economato', 'Profesor']), updateUserRole);          // Editar rol (Solo Jefe)
-router.delete('/:id', requireRole(['Jefe_Economato']), deleteUser);                // Eliminar usuario (Solo Jefe)
+router.put('/:id/reset-password', requireRole(['Jefe_Economato', 'Administrador', 'Profesor']), resetPassword); // Solo Jefe puede resetear contraseña
+router.put('/:id/role', requireRole(['Jefe_Economato', 'Administrador', 'Profesor']), updateUserRole);          // Editar rol (Solo Jefe)
+router.delete('/:id', requireRole(['Jefe_Economato', 'Administrador']), deleteUser);                // Eliminar usuario (Solo Jefe)
 
 export default router;
