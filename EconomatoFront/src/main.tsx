317fd646ba87f11304386cf0e60f2e06880bca56
Cristina <cristinarp42@gmail.com>
2026-04-25 15:22:25 +0100

    Solucionado problema buffer

--- EconomatoFront/src/main.tsx
+++ EconomatoFront/src/main.tsx
@@ -1,7 +1,11 @@
+
+import { Buffer } from 'buffer';
+(window as any).Buffer = Buffer;
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // Importamos el componente que acabamos de crear
+import App from './App' 
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- EconomatoFront/vite.config.ts
+++ EconomatoFront/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
+  optimizeDeps: {
+    include: ['buffer'],
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
